c6761ff0511f1f0a02eb1ac57d49e6f25b6987cb
JJWVersteegh <info@vertras.nl>
2026-05-02 23:30:10 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ const html = `<!DOCTYPE html>
 <html lang="nl">
 <head>
 <meta charset="UTF-8"/>
-<meta name="viewport" content="width=device-width,initial-scale=1"/>
+<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
 <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&family=Playfair+Display:ital,wght@0,700;1,400&display=swap" rel="stylesheet"/>
