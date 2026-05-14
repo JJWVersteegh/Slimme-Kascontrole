06f6de4512dfdf7a774d54681a0dfe1f66e31050
JJWVersteegh <info@vertras.nl>
2026-05-14 12:34:43 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,6 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: 'https://www.slimmekascontrole.nl', lastModified: new Date(), changeFrequency: 'monthly', priority: 1 },
-    { url: 'https://www.slimmekascontrole.nl/registreer', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.8 },
     { url: 'https://www.slimmekascontrole.nl/tarieven', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.9 },
     { url: 'https://www.slimmekascontrole.nl/vve-kascontrole', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.9 },
     { url: 'https://www.slimmekascontrole.nl/vve-kascontrole-checklist', lastModified: new Date(), changeFrequency: 'monthly', priority: 0.9 },
